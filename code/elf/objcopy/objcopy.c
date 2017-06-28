529bca0577bc842ccd3a495c8ae998a299ced5aa
promisechen <chenlx10@163.com>
2017-06-29 03:38:24 +0800

    增加写入文件的宏

--- code/elf/objcopy/objcopy.c
+++ code/elf/objcopy/objcopy.c
@@ -1,6 +1,24 @@
+#include <stdio.h>
 extern char _binary_testfile_start[];
+extern char _binary_testfile_size[];
+
+#define UNZIP_FILE(name,dstFile) \
+do\
+{\
+extern char _binary_## name ##_start[];\
+extern char _binary_## name ##_size[];\
+    printf("filesize:%lu\n",(unsigned long)_binary_## name ##_size);\
+    FILE *fp;\
+    if ((fp = fopen(dstFile, "w")) == NULL)\
+        printf("cannot open file!\n");\
+    fwrite(_binary_## name  ##_start,1,(size_t)_binary_## name ##_size,fp);\
+    fclose(fp);\
+\
+}while(0);
+
 main()
 {
-    printf("test\n");
-    printf("%s\n",_binary_testfile_start);
+    UNZIP_FILE(testfile,"a2.tar.gz");
+    getchar();
+    remove("a2.tar.gz");
 }
